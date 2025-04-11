75ed8b2068ab4ddf7b7c13d70c2d6467126aae18
Goran Saric <goransaric92@gmail.com>
2025-04-11 23:04:03 +0200

    Minor updates

--- PetAdoption/View/LoginView.swift
+++ PetAdoption/View/LoginView.swift
@@ -24,7 +24,7 @@ struct LoginView: View {
                 VStack(spacing: 20) {
                     Spacer()
                     Text("Please Login")
-                        .font(.largeTitle)
+                        .font(.title)
                         .fontDesign(.monospaced)
                         .padding()
                         .foregroundStyle(.white)
