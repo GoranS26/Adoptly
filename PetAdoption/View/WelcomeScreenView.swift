6190100bd8c3f1239d0c8ab782e3981d9ae1ad9a
Goran Saric <goransaric92@gmail.com>
2025-04-25 12:23:39 +0200

    Welcome screen adaptation

--- PetAdoption/View/WelcomeScreenView.swift
+++ PetAdoption/View/WelcomeScreenView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct WelcomeScreenView: View {
     
     @State private var pulsing = false
+    @State private var waveOffset: CGFloat = 10
+
     
 
     var body: some View {
@@ -17,43 +19,44 @@ struct WelcomeScreenView: View {
             ZStack {
                 Color.blue.opacity(0.8).ignoresSafeArea()
                 VStack{
-                    Image("background4")
-                        .resizable()
-                        .scaledToFit()
-                        .ignoresSafeArea()
-                    ZStack{
-                        Image("background4")
-                            .resizable()
-                            .scaledToFit()
-                            .ignoresSafeArea()
-                    }
+                    AnimatedWave(yOffset: waveOffset)
+                        .fill(.white)
+                        .frame(height: 450)
+                        .frame(maxHeight: .infinity, alignment: .bottom)
+                        .ignoresSafeArea(edges: .bottom)
+                        .onAppear {
+                            withAnimation(.easeInOut(duration: 2.0).repeatForever(autoreverses: true)) {
+                                waveOffset = 50
+                            }
+                        }
                 }
-                .padding(.trailing, 50)
+//                .ignoresSafeArea(edges: .bottom)
                 
                 VStack{
                     Text("ADOPTLY")
                         .foregroundStyle(.white)
                         .font(.system(size: 50, weight: .bold, design: .rounded))
                         .padding(.top)
-                        .shadow(color: .black, radius: 2)
+                        .shadow(color: .black, radius: 5)
                     
                     Image("pets")
                         .aspectRatio(contentMode: .fill)
                         .frame(width: 550)
-                        .padding(.bottom, -50)
-                        .scaleEffect(pulsing ? 1.02 : 1)
-                        .animation(Animation.easeInOut(duration: 0.6)
-                                .repeatForever(autoreverses: true), value: pulsing)
-                        .onAppear {
-                            pulsing.toggle()
-                        }
+//                        .padding(.bottom, -60)
+//                        .scaleEffect(pulsing ? 1.01 : 1)
+//                        .animation(Animation.easeInOut(duration: 0.6)
+//                                .repeatForever(autoreverses: true), value: pulsing)
+//                        .onAppear {
+//                            pulsing.toggle()
+//                        }
                     
                     Text("""
                                Find your 
                          new best friend
                          """)
                     .font(.system(size: 30, weight: .bold, design: .rounded))
-                    .foregroundStyle(.white)
+                    .foregroundStyle(.blue)
+                    .shadow(color: .black, radius: 1)
                     Spacer()
                     NavigationLink(destination: IntroductionView()){
                         Text("Get Started".uppercased())
@@ -65,7 +68,7 @@ struct WelcomeScreenView: View {
                                 .frame(width: 250, height: 50)
                                 .background(Color.blue.opacity(0.9))
                                 .cornerRadius(25)
-                                .shadow(color: .black, radius: 10)
+                                .shadow(color: .black, radius: 5)
                                 
                     }
                     .padding(.top, 75)
@@ -75,15 +78,41 @@ struct WelcomeScreenView: View {
                     
                 }
             }
-            
-           
-            
         }
         .navigationBarBackButtonHidden(true)
+    }
+    
+    struct AnimatedWave: Shape {
+        
+        var yOffset: CGFloat // controls the wave height
+        
+        // This makes the shape animatable
+        var animatableData: CGFloat {
+            get { yOffset }
+            set { yOffset = newValue }
+        }
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+            path.move(to: CGPoint(x: 0, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: 100))
+            
+            
+            path.addCurve(to: CGPoint(x: rect.width, y: 75 - yOffset),
+                          control1: CGPoint(x: rect.width * 0.50, y: 50 - yOffset),
+                          control2: CGPoint(x: rect.width * 0.75, y: 50 + yOffset))
+            
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.closeSubpath()
+            
+            return path
+        }
     }
 }
 
 #Preview {
     WelcomeScreenView()
 }
+
 
